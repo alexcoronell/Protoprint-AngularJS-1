e58ee6ef3db48787db59db85fee77051bc425f44
Alex Coronell <alexcoronell@gmail.com>
2019-10-17 18:23:29 -0500

    Primer Commit

---
+++ js/rutas.js
@@ -0,0 +1,48 @@
+<<<<<<< HEAD
+var proto = angular.module('protoprint', ['ngRoute']);
+
+proto.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/quienessomos', {
+            templateUrl: 'pages/quienessomos.html',
+        }).
+        when('/servicios', {
+            templateUrl: 'pages/servicios.html',
+        }).
+        when('/catalogos', {
+            templateUrl: 'pages/catalogo.html',
+        }).
+        when('/contactanos', {
+            templateUrl: 'pages/contactanos.html',
+        }).
+        otherwise({
+            redirectTo: '/',
+            templateUrl: 'pages/inicio.html',
+        });
+    }
+=======
+var proto = angular.module('protoprint', ['ngRoute']);
+
+proto.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/quienessomos', {
+            templateUrl: 'pages/quienessomos.html',
+        }).
+        when('/servicios', {
+            templateUrl: 'pages/servicios.html',
+        }).
+        when('/catalogos', {
+            templateUrl: 'pages/catalogo.html',
+        }).
+        when('/contactanos', {
+            templateUrl: 'pages/contactanos.html',
+        }).
+        otherwise({
+            redirectTo: '/',
+            templateUrl: 'pages/inicio.html',
+        });
+    }
+>>>>>>> b3ab241a4da8f29a63ea4caba22659e6368ce90c
+]);
---
+++ js/site.js
@@ -0,0 +1,100 @@
+<<<<<<< HEAD
+//---------------------------- SLIDER PRINCIPAL PAGINA INICIO ----------------------------------------
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoWidth: true,
+    navText: ['<i class="fa fa-chevron-circle-left" title="Anterior"></i>', '<i class="fa  fa-chevron-circle-right" title="Siguiente"></i>'],
+    responsive: {
+        0: {
+            items: 1
+        },
+        500: {
+            items: 2,
+            margin: 20
+        },
+        800: {
+            items: 3,
+            margin: 20
+        },
+        1000: {
+            items: 4,
+            margin: 20
+        }
+    }
+})
+
+
+
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 0,
+//     nav: true,
+//     autoWidth: false,
+//     navText: ['<i class="fa fa-chevron-circle-left" title="Anterior"></i>', '<i class="fa  fa-chevron-circle-right" title="Siguiente"></i>'],
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         768: {
+//             items: 2,
+//             margin: 20
+//         },
+//         1200: {
+//             items: 3,
+//             margin: 20
+//         },
+//     }
+=======
+//---------------------------- SLIDER PRINCIPAL PAGINA INICIO ----------------------------------------
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoWidth: true,
+    navText: ['<i class="fa fa-chevron-circle-left" title="Anterior"></i>', '<i class="fa  fa-chevron-circle-right" title="Siguiente"></i>'],
+    responsive: {
+        0: {
+            items: 1
+        },
+        500: {
+            items: 2,
+            margin: 20
+        },
+        800: {
+            items: 3,
+            margin: 20
+        },
+        1000: {
+            items: 4,
+            margin: 20
+        }
+    }
+})
+
+
+
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 0,
+//     nav: true,
+//     autoWidth: false,
+//     navText: ['<i class="fa fa-chevron-circle-left" title="Anterior"></i>', '<i class="fa  fa-chevron-circle-right" title="Siguiente"></i>'],
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         768: {
+//             items: 2,
+//             margin: 20
+//         },
+//         1200: {
+//             items: 3,
+//             margin: 20
+//         },
+//     }
+>>>>>>> b3ab241a4da8f29a63ea4caba22659e6368ce90c
+// })
